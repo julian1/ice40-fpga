ebf8e0b21dc473a85901d2f534db9ddabd06128b
julian1 <mail@julian1.io>
2021-03-29 08:06:51 +1100

    update

--- example-smu09/example.v
+++ example-smu09/example.v
@@ -10,6 +10,38 @@
 // versus a 16 bit reg value
 // versus propagating.
 
+
+module blinker    (
+  input  clk,
+  input  cs,
+  input  d
+
+);
+
+  localparam BITS = 5;
+  // localparam LOG2DELAY = 21;
+  localparam LOG2DELAY = 19;
+
+  reg [BITS+LOG2DELAY-1:0] counter = 0;
+  reg [BITS-1:0] outcnt;
+
+  always@(posedge clk) begin
+    counter <= counter + 1;
+    outcnt <= counter >> LOG2DELAY;
+  end
+
+  // assign {LED1} = counter2 >> 22;
+  // assign { LED1, LED2, LED3, LED4, LED5 } = outcnt ^ (outcnt >> 1);
+  // assign {  LED1, LED2 } = outcnt ^ (outcnt >> 1);
+
+
+
+
+endmodule
+
+
+
+
 module mymux   #(parameter MSB=8)   (
   input  clk,
   input  cs,
@@ -26,7 +58,8 @@ module mymux   #(parameter MSB=8)   (
 
    always @ (posedge clk)
       if (!cs)
-         out <= 0;
+         out <= 0;        // this clears after de-assert... which is not what we want... 
+                          // instead we want nop.
       else begin
          if (en)
             out <= {out[MSB-2:0], d};
@@ -36,6 +69,7 @@ module mymux   #(parameter MSB=8)   (
 endmodule
 
 
+// The above should be good enough to test ... just wire the led to the lsb. 
 
 
 module top (
@@ -67,23 +101,6 @@ module top (
     .out(out)
   );
 
-
-
-  localparam BITS = 5;
-  // localparam LOG2DELAY = 21;
-  localparam LOG2DELAY = 19;
-
-  reg [BITS+LOG2DELAY-1:0] counter = 0;
-  reg [BITS-1:0] outcnt;
-
-  always@(posedge clk) begin
-    counter <= counter + 1;
-    outcnt <= counter >> LOG2DELAY;
-  end
-
-  // assign {LED1} = counter2 >> 22;
-  // assign { LED1, LED2, LED3, LED4, LED5 } = outcnt ^ (outcnt >> 1);
-  // assign {  LED1, LED2 } = outcnt ^ (outcnt >> 1);
 
 
   assign LED1 = SCLK;
