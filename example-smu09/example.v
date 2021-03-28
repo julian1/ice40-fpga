509566f1e360823b9e1c487435eefefa55a63396
julian1 <mail@julian1.io>
2021-03-29 09:30:00 +1100

    ok all output spi pins working

--- example-smu09/example.v
+++ example-smu09/example.v
@@ -110,9 +110,10 @@ module top (
   assign { LED1, LED2 } = out;    // lowest bits or highest?
 */
 
+  // want to swapt the led order.
+  assign LED1 = MOSI;
 
-
-
+/*
   blinker #(  )
   blinker
     (
@@ -120,7 +121,7 @@ module top (
     .led1(LED1),
     .led2(LED2)
   );
-
+*/
 
 
 
