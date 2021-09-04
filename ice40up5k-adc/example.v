b32ce951e26aa67b315eff9221264abfaec2bc5a
julian1 <git@julian1.io>
2021-09-04 11:59:50 +1000

    update

--- ice40up5k-adc/example.v
+++ ice40up5k-adc/example.v
@@ -13,10 +13,14 @@ module top (
   reg [BITS+LOG2DELAY-1:0] counter = 0;
   reg [BITS-1:0] outcnt;
 
+
   always@(posedge clk) begin
     counter <= counter + 1;
     outcnt <= counter >> LOG2DELAY;
   end
+
+  // assign { LED_R, LED_G, LED_B } = outcnt ^ (outcnt >> 1);
+
 
 
   //////////////////////////////////////////////////////
@@ -24,15 +28,24 @@ module top (
   reg [31:0] count = 0;
  
 
-  // assign { LED_R, LED_G, LED_B } = outcnt ^ (outcnt >> 1);
  
   // 0 is on or off? 
   // 0 == all bits off, turns leds on.
-  reg [2:0] leds = 3'b111;
-  assign { LED_R, LED_G, LED_B } = leds;
+  // reg [2:0] leds = 3'b111;    // all off.
+
+  reg [2:0] leds = 3'b101;        // middle on.
 
 
-  `define STATE_INIT 0    // initialsation state
+
+  // assign { LED_R, LED_G, LED_B } = counter >> 20 ;  // this works???
+  // assign { LED_R, LED_G, LED_B } = counter >> 21 ;  // this works???
+  // assign { LED_R, LED_G, LED_B } = count >> 21 ;      // ok. working. if remove the case block.. 
+
+
+  assign { LED_R, LED_G, LED_B } = leds;      
+
+
+  `define STATE_INIT    0    // initialsation state
   // `define STATE_WAITING 1
   `define STATE_PREF    2
   `define STATE_NREF    3
@@ -51,17 +64,7 @@ module top (
           begin
             // initialize count
             count <= 0;
-          /*
-            // init defaults
-            short_count <= 10000;     // 10ms approx
-            runup_count <= 4000000;   // 1m - 0.1 sec approx
-            m_short <= 0;
-            m_in <= 0;
-
-            // set defaults
-            state <= `STATE_WAITING;
-      */
-            state <= STATE_PREF;
+            state <= `STATE_PREF;
           end
 
       
@@ -69,12 +72,12 @@ module top (
           begin
             // should use dedicated pref count... and accumulate.
             // or have a count dedicated....
-            leds = 3'b100;
 
-            if(count == 20000000 )
+            if(count == 2000000 )
               begin
                 // swap to reference input for rundown
                 state <= `STATE_NREF;
+                // leds <= 3'b100;
 
               end
           end
@@ -83,12 +86,11 @@ module top (
         `STATE_NREF:
           begin
 
-            leds = 3'b010;
-
-            if(count == 40000000 )
+            if(count == 4000000 )
               begin
                 // swap to reference input for rundown
                 state <= `STATE_INIT;
+                // leds <= 3'b001;
               end
           end
 
