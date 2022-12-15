0bc99fb15f0bf109f4b391d76dd14f2e8a758eb6
julian1 <mail@julian1.io>
2022-12-15 20:59:15 +1100

    update

--- smu/build.sh
+++ smu/build.sh
@@ -1,32 +1,33 @@
+
+
+
+
 ##!/bin/bash
 
+# see https://github.com/damdoy/ice40_ultraplus_mains/tree/master/leds
 
-# nix-shell -p yosys arachne-pnr icestorm usbutils
-# nix-shell ~/nixos-config/examples/icestorm.nix
-
-# ./build.sh  2>&1  | grep warning
-
-# trap for failure
 set -e
 
 rm -rf ./build
 mkdir ./build
 
 
-yosys -p 'synth_ice40 -top top -blif ./build/main.blif' main.v 
+# yosys -p "synth_ice40  -top top  -blif ./build/main.blif" main.v
+# arachne-pnr -d 5k -P sg48 -p main.pcf  ./build/main.blif -o ./build/main.asc
 
-arachne-pnr -d 1k -o ./build/main.asc -p main.pcf ./build/main.blif 
+yosys -p "synth_ice40  -top top  -json ./build/main.json" main.v  2>&1 | tee ./build/yosys.txt
+
+
+nextpnr-ice40 --hx1k  --package  tq144 --pcf  main.pcf --json ./build/main.json  --asc  ./build/main.asc 2>&1 | tee ./build/nextpnr.txt
 
 
 icepack ./build/main.asc ./build/main.bin
 
-icetime ./build/main.asc -d hx1k
+icetime ./build/main.asc -d hx1k 2>&1 | tee ./build/icetime.txt
 
-#
+
+
 echo "finished"
 
-# send to device - 
-# TODO pass arg?
-# iceprog ./build/example.bin
 
 
