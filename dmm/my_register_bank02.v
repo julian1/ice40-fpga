21fddb344e96dd50f05e5b2a8eea04e51107e650
julian1 <git@julian1.io>
2023-06-28 10:17:57 +1000

    ok, got 4094 relay driving working again

--- dmm/my_register_bank02.v
+++ dmm/my_register_bank02.v
@@ -7,13 +7,17 @@
     but simpler - is to just padd an extra byte an use a couple of bits.
 */
 
+
+/*
+  if we add back the bitwise set,clear,toggle, for a 8 bit reg, then could aggrevate registers if we wanted.
+    error flags etc.
+
+*/
+
 `define REG_LED                 7
 `define REG_SPI_MUX             8
 `define REG_4094                9
 
-
-// could also
-`define REG_WHOOT               9
 
 
 
@@ -30,10 +34,12 @@ module my_register_bank02   #(parameter MSB=40)   (
 
   // control read/write control vars
   // use ' inout',  must be inout to write
+
+  // todo. consider adding bitwidth in name.
   inout wire [24-1:0] reg_led ,
 
   output reg [24-1:0] reg_spi_mux,
-  output reg [24-1:0] reg_4094,
+  output reg [24-1:0] reg_4094,     // TODO change name status .  or SR. reg_4094_.   or SR_4094,   sr_4094.
 
 
 );
@@ -49,7 +55,9 @@ module my_register_bank02   #(parameter MSB=40)   (
 
   // To use in an inout. the initial block is a driver. so must be placed here.
   initial begin
-    reg_led             = 5'b10101;
+    reg_led       = 5'b10101;   // 24'b10101 ? should specify bitwidth
+    reg_spi_mux   = 0;          // no spi device active
+    reg_4094      = 0;
 
   end
 
@@ -80,18 +88,18 @@ module my_register_bank02   #(parameter MSB=40)   (
         count <= count + 1;
 
         /*
-        // we MUST read 8 bits here, to have the lsb bits of the register address.
-            but this creates issue for how quickly we can stuff data into dout, so value can be read in xfer
-            one option is to change to non blocking.
-            but simpler - is to just pad an extra byte, and accept loss of some of the higher bits
+        // we MUST read 8 bits here, in order to get the lsb bits of the register address.
+            but this creates issue for how quickly we can stuff data into dout, to be read by the master on the clk cycle,
+            one possibility is to use blocking assignment.
+            but better/simpler - is to add extra byte, and accept that some of the higher bits are lost
         */
         if(count == 8)
           begin
             case (in[8 - 1 - 1: 0 ] )
 
-
               `REG_LED:       out <= reg_led  << 9;
-
+              `REG_SPI_MUX:   out <= reg_spi_mux << 9;
+              `REG_4094:      out <= reg_4094 << 9;
               default:        out <= 12345;
 
             endcase
@@ -119,13 +127,15 @@ module my_register_bank02   #(parameter MSB=40)   (
         if ( flag == 0  )  // 0 means write.
           case (addr)
 
-            `REG_LED:   reg_led <= val24;
+            `REG_LED:       reg_led <= val24;
+            `REG_SPI_MUX:   reg_spi_mux <= val24;
+            `REG_4094:      reg_4094 <= val24;
 
           endcase
       end
 
     // we could handle bit set/clear/toggle updates here, if we wanted, for 8 bit registers.
-    else if( count == 32 ) 
+    else if( count == 32 )
       begin
 
       end
