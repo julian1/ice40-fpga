14b34ce31edadbd82b8186f74c643fe53f97f0e6
julian1 <git@julian1.io>
2023-09-01 10:15:48 +1000

    led blinking. mux blinking, board dsn 232
    pin rename
    
    led blinking. board dsn 232
    
    and got clk on mon0.
    
    blinky fix pin assignemnts

--- hx4k-blinky/main.v
+++ hx4k-blinky/main.v
@@ -1,26 +1,29 @@
 
 
 module top (
-  input  clk,
-  output LED1,
-  output LED2,
+  input  CLK,
 
+  output LED0,
+  // output LED2,
+
+  output MON0,
   output MON1,
   output MON2,
   output MON3,
   output MON4,
   output MON5,
-  output MON6,   
-  output MON7,   
+  output MON6,
+  output MON7,
 
+  output U413_EN_CTL,
   output U413_A0_CTL,
   output U413_A1_CTL,
   output U413_A2_CTL,
 
+  output U902_SW0_CTL,
   output U902_SW1_CTL,
   output U902_SW2_CTL,
   output U902_SW3_CTL,
-  output U902_SW4_CTL,
 
 );
 
@@ -30,26 +33,29 @@ module top (
   reg [BITS+LOG2DELAY-1:0] counter = 0;
   reg [BITS-1:0] outcnt;
 
-  always@(posedge clk) begin
+  always@(posedge CLK) begin
     counter <= counter + 1;
     outcnt <= counter >> LOG2DELAY;
   end
 
-  // assign { LED1, LED2} = outcnt ^ (outcnt >> 1);
-  assign { LED2, LED1} = outcnt ^ (outcnt >> 1);
-  
+  // assign { LED0, LED2} = outcnt ^ (outcnt >> 1);
+  // assign { LED2, LED0} = outcnt ^ (outcnt >> 1);
+  assign {  LED0} = outcnt ^ (outcnt >> 1);
 
 
 
+  assign { U902_SW3_CTL, U902_SW2_CTL, U902_SW1_CTL , U902_SW0_CTL } = outcnt ^ (outcnt >> 1);
 
 
-  assign MON1 = clk ;   // note the slight skew. from input clock due popagation delay.
+  assign { U413_A2_CTL, U413_A1_CTL, U413_A0_CTL, U413_EN_CTL } = outcnt ^ (outcnt >> 1);
+
+
+
+  assign MON0 = CLK ;   // note the slight skew. from input clock due popagation delay.
 
   assign { MON4, MON3, MON2 } = counter;
 
-  assign { U413_A2_CTL, U413_A1_CTL, U413_A0_CTL } = counter;
 
-  assign { U902_SW4_CTL, U902_SW3_CTL, U902_SW2_CTL, U902_SW1_CTL} = counter;
 
 endmodule
 
