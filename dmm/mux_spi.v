c27cf5e6359c1cd9e13a02717509ea8ef6e0efeb
julian1 <git@julian1.io>
2023-09-24 10:54:11 +1000

    squash update   - ok comms established, relay works, montitor shows pattern, and and can nge the pattern, by writing a reg.   - trick is not to use the upper two bits of the register.
    / rebase - ok. working. test03 to 4094 flip relay, and write register to control led
    
    rename macro
    
    rename reg_direct in main.v
    
    very odd. assignment issue
    
    adding a read fucks it
    
    weird failure, with REG_MODE
    
    ok. it all works. so long as we dont pass in the real reg-mode
    
    appears working
    
    ok, mode is being passes to mux assign ok, when all patterns are the same
    
    tidy
    
    working with two inputs to muxer
    
    rename reg_redirect
    
    add reg_direct
    
    assign conditioning_out directly to test_pattern works
    
    bizzare
    
    ok. working. test03 to 4094 flip relay, and write register to control led
    
    rename
    
    still working for start, test03 relay, test04 led blink. but theres a failure when dont assign led. very odd
    
    ok. when we changed to reg_mode from two bits to 24 bits it worked
    
    ok when change reg_spi_mux from 8 to 24 bits, then the spi relay toggle works again
    
    update
    
    update
    
    wow. when changed select from default 32bit integer to 24 bit it worked, wow
    
    comment
    
    ok fixed all wire size warnings
    
    update
    
    give reset to test pattern
    
    update
    
    ok, good, led flashing, in reset
    
    tidy
    
    ok assigning default value to blink led, and relay ok.
    
    ok. got monitor working with test pattern
    
    relay doesnt work, although have test pattern
    
    fix relay
    
    ok. we are flipping between the test pattern and direct write
    
    starts, relay works, and mon works, and off and on
    
    ok. starts, relay works, mon first 3 pins are good
    
    restrict until ok.
    
    weirdness, wriging some monitor pins wrecks the 4094
    
    update, starts, relay works, can alternate test pattern
    
    update
    
    comment
    
    shrink the vctor down, starts, relay works, but our register value is not being accepted
    
    ok starts, relay works, and all monitor pins work
    
    and use the correct monitor pins, and it works!

--- dmm/mux_spi.v
+++ dmm/mux_spi.v
@@ -5,7 +5,11 @@
 
 
 module mux_spi    (
-  input [8-1:0] reg_spi_mux,     // change name vec_active_device
+
+  // everything is a wire.
+
+  // input [8-1:0] reg_spi_mux,     // change name vec_active_device
+  input [24-1:0] reg_spi_mux,     // change name vec_active_device
   input cs2,
   input clk,
   input mosi,
