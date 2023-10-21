8ce74acea250202c3396e744751bbe74ab4efe0c
julian1 <mail@julian1.io>
2023-10-21 16:26:42 +1100

    tidy

--- dmm/modulation_az.v
+++ dmm/modulation_az.v
@@ -141,13 +141,6 @@ module modulation_az (
           if(adc_take_measure_done == 1)
             state <= 4;
 
-          // wait for adc.
-          // if(clk_count_down == 0)
-          //   state <= 4;
-
-
-          // if(clk_count_down == 0)
-          //   state <= 4;
 
         // switch pre-charge switch back to boot to protect signal again
         4:
@@ -181,10 +174,6 @@ module modulation_az (
             state <= 6;
 
 
-          // if(clk_count_down == 0)
-          //  state <= 6;
-
-
         6:
           if(run )        // place at end.
             state <= 2;
@@ -201,16 +190,15 @@ module adc (
 
   input   clk,
   input   reset,
-
   input [ 32-1 : 0 ] clk_sample_duration,  // 32/31 bit nice. for long sample....  wrongly named it is counter_sample_duration. not clk...
-
   input adc_take_measure,  // wire
+
   output reg adc_take_measure_done
 
 );
 
-  reg [7-1:0]   state = 0 ;     // should expose in module, not sure.
-  reg [31:0]    clk_count_down;           // clk_count for the current phase. using 31 bitss, gives faster timing spec.  v 24 bits. weird. ??? 36MHz v 32MHz
+  reg [7-1:0]   state = 0 ;
+  reg [31:0]    clk_count_down;
 
 
   always @(posedge clk  or posedge reset )
@@ -230,11 +218,9 @@ module adc (
 
       case (state)
 
-        // precharge switch - protects the signal. from the charge-injection of the AZ switch.
         0:
           // having a state like, this may be useful for debuggin, because can put a pulse on the monitor.
           state <= 2;
-
 
         /////////////////////////
 
@@ -243,21 +229,20 @@ module adc (
           if(adc_take_measure == 1)
             state <= 3;
 
-
         3:
+          // set up state for measurement
           begin
             state           <= 35;
             clk_count_down  <= clk_sample_duration;
           end
 
         35:
-          // wait for measurement
+          // wait for measurement to complete
           if(clk_count_down == 0)
             state <= 4;
 
-
-        // switch pre-charge switch back to boot to protect signal again
         4:
+          // measure done
           begin
             state           <= 5;
             // signal measurement done
