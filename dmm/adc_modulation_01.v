10e068d145b909604855ccc2c361c573be027dff
julian1 <mail@julian1.io>
2023-11-01 11:21:49 +1100

    rename var

--- dmm/adc_modulation_01.v
+++ dmm/adc_modulation_01.v
@@ -136,7 +136,7 @@ module adc_modulation (
   // both should be input wires. both are driven.
   // input           com_interrupt,
   // output reg      com_interrupt,  // JA remove.
-  output reg      cmpr_latch_ctl,
+  output reg      cmpr_latch_disable_ctl,
 
 
   ///////////
@@ -181,7 +181,7 @@ module adc_modulation (
 
     // com_interrupt    = 1; // active lo move this to an initial condition.
 
-    cmpr_latch_ctl  = 1; // disable comparator,
+    cmpr_latch_disable_ctl  = 1; // disable comparator,
 
   end
 
@@ -465,20 +465,20 @@ module adc_modulation (
             refmux        <= `MUX_REF_POS; // initial direction
 
 
-            cmpr_latch_ctl  <= 0; // enable comparator, // JA correct. 0 means it is transparent.
+            cmpr_latch_disable_ctl  <= 0; // enable comparator, // JA correct. 0 means it is transparent.
           end
 
         `STATE_FIX_POS:
           /*
           // half way through first fix, enable the comparator
           if(clk_count >= (p_clk_count_fix >> 1))
-            // cmpr_latch_ctl  <= 0
+            // cmpr_latch_disable_ctl  <= 0
           */
           if(clk_count >= p_clk_count_fix)       // walk up.  dir = 1
             begin
               state <= `STATE_VAR_START;
 
-              // cmpr_latch_ctl  <= 0; // enable comparator, we test the comparator_last value in the next clock cycle - not enough time...
+              // cmpr_latch_disable_ctl  <= 0; // enable comparator, we test the comparator_last value in the next clock cycle - not enough time...
 
             end
 
@@ -682,7 +682,7 @@ module adc_modulation (
             if(cross_any )
               begin
 
-                cmpr_latch_ctl          <= 1; // disable comparator,
+                cmpr_latch_disable_ctl          <= 1; // disable comparator,
 
                 // trigger for scope
                 // transition
@@ -763,7 +763,7 @@ module adc_modulation (
             sigmux          <= 0;
             refmux          <= `MUX_REF_RESET;
 
-            cmpr_latch_ctl          <= 1; // // disable comparator, enable latch
+            cmpr_latch_disable_ctl          <= 1; // // disable comparator, enable latch
 
             // monitor     <=  6'b000000;    // indicate we have started.
             monitor     <=  6'b000001;    // indicate we have started.
