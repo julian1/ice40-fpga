aafdfa5fdeb6b02460a7cf6cdaa0a38decef93e0
julian1 <mail@julian1.io>
2023-11-17 11:59:42 +1100

    comment

--- dmm/main.v
+++ dmm/main.v
@@ -32,8 +32,8 @@
 // `include "adc_modulation_02.v"
 // `include "adc_modulation_03.v"
 // `include "adc_modulation_04.v"
-// `include "adc_modulation_05.v"
 `include "adc_modulation_05.v"
+// `include "adc_modulation_06.v"
 
 
 
