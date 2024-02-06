9992802ab679815021780f8f8a6efd18c1bef08e
julian1 <mail@julian1.io>
2024-02-07 08:06:29 +1100

    rename top.v

--- minimal/build-4k.sh
+++ minimal/build-4k.sh
@@ -11,10 +11,10 @@ rm -rf ./build
 mkdir ./build
 
 
-# yosys -p "synth_ice40  -top top  -blif ./build/main.blif" main.v
+# yosys -p "synth_ice40  -top top  -blif ./build/main.blif" top.v
 # arachne-pnr -d 5k -P sg48 -p main.pcf  ./build/main.blif -o ./build/main.asc
 
-yosys -p "synth_ice40  -top top  -json ./build/main.json" main.v  2>&1 | tee ./build/yosys.txt
+yosys -p "synth_ice40  -top top  -json ./build/main.json" top.v  2>&1 | tee ./build/yosys.txt
 
 egrep -i 'warning|error' ./build/yosys.txt  > ./build/yosys-errors.txt
 
