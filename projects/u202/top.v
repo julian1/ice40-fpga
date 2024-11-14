577b617ea49e3c545cf43a6e494dd26dcde29d93
julian1 <mail@julian1.io>
2024-11-14 15:30:08 +1100

    squash update, add buzzer
    ok
    
    squash update, ok fan and buzzer ok
    
    fan works, buzzer doesnt
    
    update
    
    update
    
    ok works
    
    update
    
    update
    
    squash tidy
    
    tidy

--- projects/u202/top.v
+++ projects/u202/top.v
@@ -8,54 +8,32 @@
 */
 
 
-
-/*
-`include "../../common/mux_assign.v"
-`include "../../common/test_pattern.v"
-`include "../../common/timed_latch.v"
-
-*/
 `include "register_set.v"
 
-/*
-`include "adc-mock.v"
-`include "refmux-test.v"
 
-`include "adc_modulation_05.v"
-`include "sequence_acquisition.v"
-
-*/
 `default_nettype none
 
 
 
 
-// `define CLK_FREQ        20000000
+// 12MHz
 `define CLK_FREQ        12000000
 
 
-
+// change name simple Timer state or something.
+// or square_wave or something.
 
 module fet_driver (
 
   input   clk,
   input   reset_n,
 
-  output reg [ 4-1: 0 ] fets_o,
+  input  [ 32-1: 0 ] p_clk_count_n,
+
+  output reg [ 4-1: 0 ] out,
 );
-
-
   reg [7-1:0]   state = 0 ;
   reg [31:0]    clk_count_down;
-
-  /*
-    there's a very strange bug lurking here, if we remove this register allocation , that is not even used.
-    then get spurious signals on adc_measure_valid in the real adc mode.
-
-  */
-
-
-
 
   always @(posedge clk   )
     begin
@@ -64,35 +42,30 @@ module fet_driver (
         // always decrement clk for the current phase
       clk_count_down <= clk_count_down - 1;
 
-
       case (state)
 
         0:
-          // reset/park state.
           begin
-
             // setup next
             clk_count_down  <= 10;     // set positive to avoid wrap around
             state           <= 1;
-            fets_o          <= 3'b0000;    // off.
+            out          <= 3'b0000;    // off.
           end
 
         1:
           if(clk_count_down == 0)
             begin
-              clk_count_down  <= `CLK_FREQ / (15000 * 2);   // 15kHz.
-              // clk_count_down  <= `CLK_FREQ / 1000;   // 500Hz..   fet bootstrap cap not enough charge.
+              clk_count_down  <= p_clk_count_n;
               state           <= 2;
-              fets_o          <= 4'b1001;    // fet1 (hi), fet4
+              out          <= 4'b1001;    // fet1 (hi), fet4
             end
 
         2:
           if(clk_count_down == 0)
             begin
-              clk_count_down  <= `CLK_FREQ / (15000 * 2);   // 15kHz.
-              // clk_count_down  <= `CLK_FREQ / 1000;    // 500Hz.
+              clk_count_down  <= p_clk_count_n;
               state           <= 1;
-              fets_o          <= 4'b0110;    // fet3 (hi), fet2
+              out          <= 4'b0110;    // fet3 (hi), fet2
             end
 
       endcase
@@ -101,11 +74,52 @@ module fet_driver (
       // synchronous reset - if reset_n enabled, then don't advance out-of reset state.
       if(reset_n == 0)      // in reset
         begin
-
             state <= 0;
         end
+    end
+endmodule
 
 
+
+
+module fan_driver (
+
+  input   clk,
+  input   reset_n,
+
+  // EXTR. there are issues at start/ if we initialize output at full power.
+  output reg  out = 1,    // off, turn fet on, pull out lo.
+);
+
+  // fan DS example uses 25kHz.
+
+  reg [31:0]    clk_count_up  = 0;
+
+  always @(posedge clk   )
+    begin
+
+      clk_count_up <= clk_count_up + 1;
+
+      // turn on pulse/
+      if(clk_count_up == `CLK_FREQ  / 25000)  // 480
+        begin
+          clk_count_up <= 0;
+          out <= 1;
+        end
+
+      // finish pulse.  a clock_count up would be easier
+      // shorter-pulse is more faster
+      // longer-pulse - is less power/slower
+      if(clk_count_up == 400 )
+        begin
+          out <= 0;
+        end
+
+      // synchronous reset - if reset_n enabled,
+      if(reset_n == 0)      // in reset
+        begin
+            out <= 1;
+        end
     end
 endmodule
 
@@ -150,26 +164,61 @@ module top (
 
   // input  SPI_CS2,
 
+  input clk,
+
 
   ///////////
   output [ 4-1: 0 ] fets_o,
+  output [ 2-1: 0 ] buzzer_o,
 
-  input clk
+  output  fan_pwm_o,
+  input   fan_tach_i
+
 
 );
 
   reg [32-1:0] reg_status = 0;    // initial value
 
+  wire [32-1:0] reg_direct ;    // can set initial value. initial value
 
+
+  // fets
   fet_driver
-  fet_driver(
+  fet_driver1(
     . clk( clk),
     . reset_n( 1'b1 ),
-    . fets_o( fets_o),
+
+    . p_clk_count_n( `CLK_FREQ / (15000 * 2)  ),    // 15kHz.
+
+    . out( fets_o),
   );
 
-  // wire SDO = 1;
 
+  reg [2:1] dummy2;
+
+  // buzzer
+  fet_driver
+  buzzer(
+    . clk( clk),
+    . reset_n( reg_direct[0]  ),     // first bit controls
+    . p_clk_count_n( `CLK_FREQ / (4096 * 2)  ),    // 4kHz.
+    . out( { dummy2 , buzzer_o } ),
+  );
+
+
+  // I think there's a resize issue.
+
+
+  fan_driver
+  fan_driver(
+    . clk( clk),
+    . reset_n( 1'b1 ),
+    . out( fan_pwm_o),
+  );
+
+
+  // wire fan_pwm_o = 0;   // fan full speed.
+  // wire fan_pwm_o = 1;     // fan off
   register_set // #( 32 )
   register_set
     (
@@ -183,7 +232,7 @@ module top (
 
 
     // outputs
-    // . reg_direct( fets_o ),
+    . reg_direct( reg_direct ),
 
     // inputs
     . reg_status( reg_status),
