57aa0b43ecd46945bf0b8455f6ce636057d3a21b
julian1 <mail@julian1.io>
2025-12-11 17:24:00 +1100

    comment

--- projects/minimal/register_set.v
+++ projects/minimal/register_set.v
@@ -46,7 +46,7 @@
 //  sample acquisition.
 `define REG_SA_P_CLK_COUNT_PRECHARGE    20
 
-`define REG_SA_P_SEQ_N                  21      // TODO - consider reorder
+`define REG_SA_P_SEQ_N                  21
 `define REG_SA_P_SEQ0                   22
 `define REG_SA_P_SEQ1                   23
 `define REG_SA_P_SEQ2                   24
