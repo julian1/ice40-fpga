0d19cc683d28a2f12c6c9cb24a01071d11b08bca
julian1 <git@julian1.io>
2026-05-23 09:41:40 +1000

    fix defines

--- projects/minimal/defines.v
+++ projects/minimal/defines.v
@@ -15,6 +15,19 @@
   helpful to populate the registers with some default vals for default test/start behaviors
 */
 
+`define SOFF        0
+// `define S1          ((1-1)<<1|1'b1)     // { 3'b0, 1'b1 }
+`define S1          { 3'd0, 1'b1 }
+`define S2          { 3'd1, 1'b1 }
+`define S3          { 3'd2, 1'b1 }
+`define S4          { 3'd3, 1'b1 }
+`define S5          { 3'd4, 1'b1 }
+`define S6          { 3'd5, 1'b1 }
+`define S7          { 3'd6, 1'b1 }
+`define S8          { 3'd7, 1'b1 }
+
+
+/*
 `define S1          ((1<<3)|(1-1))
 `define S2          ((1<<3)|(2-1))
 `define S3          ((1<<3)|(3-1))
@@ -24,4 +37,4 @@
 `define S7          ((1<<3)|(7-1))
 `define S8          ((1<<3)|(8-1))
 
-
+*/
